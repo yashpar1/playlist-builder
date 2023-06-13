efc418d2541632bc5693d61f22b66f9072e85d07
Yashwant Parmar <yashwant.parmar@gmail.com>
2023-06-13 16:12:42 -0700

    Began adding Flask to allow JS to call function

--- javascript/src/clustering.py
+++ javascript/src/clustering.py
@@ -1,4 +1,5 @@
 import sys, pandas as pd
+from flask import Flask
 from sklearnex import patch_sklearn
 patch_sklearn()
 from sklearn import preprocessing as pre
@@ -6,15 +7,18 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from itertools import combinations
 
+clusterer = Flask(__name__)
+
 if not sys.warnoptions:
     import warnings
     warnings.simplefilter("ignore")
 
 features = ['energy', 'tempo', 'danceability', 'valence']
 
-def feature_extraction(ids, feats):
+@clusterer.route("/clusterer", methods=['GET', 'POST'])
+def song_clustering(ids, feats):
     """
-    Creates Pandas DataFrame of song ids and features, norms features
+    Creates Pandas DataFrame of song ids and features, norms features, clusters songs
     """
     
     df_feats = pd.DataFrame(ids).join(pd.DataFrame(feats)[[features]])
@@ -22,14 +26,7 @@ def feature_extraction(ids, feats):
     scaler = pre.MinMaxScaler()
     normed = scaler.fit_transform(to_norm)
     df_feats = df_feats.drop(features, axis=1).join(pd.DataFrame(normed, columns=features))
-    return df_feats
-
-def playlist_creation(dataframe_features):
-    """
-    Clusters the songs in the playlist and creates new playlists from those clusters. Tests 6 different feature combinations.
-    """
-
-    df_clustering = dataframe_features.set_index('id')
+    df_clustering = df_feats.set_index('id')
 
     n_clusters = [2, 3, 4, 5, 6]
     cols = list(df_clustering)
@@ -54,11 +51,8 @@ def playlist_creation(dataframe_features):
     feat_2 = best['feat_2'].values[0]
     num_clusts = best['clusters'].values[0]
 
-    df_to_cluster = dataframe_features[['id', feat_1, feat_2]]
+    df_to_cluster = df_feats[['id', feat_1, feat_2]]
     model = KMeans(n_clusters=num_clusts).fit(df_to_cluster[[feat_1, feat_2]].values)
     clusters = {'cluster': model.labels_}
     df_clustered = df_to_cluster.join(pd.DataFrame(clusters))
     return df_clustered
-
-def to_javascript(df_clustered):
-    pass
