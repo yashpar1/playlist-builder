7cde656a8e198c45917f76d8d330bdc2a0918905
Yashwant Parmar <yashwant.parmar@gmail.com>
2023-10-03 12:01:56 -0700

    Fixed generator function and its caller function

--- javascript/src/songInfo.js
+++ javascript/src/songInfo.js
@@ -3,43 +3,38 @@ function fixUris(uri) {
   return fixed;
 };
 
-async function* getSongs(uri, token) {
+function* getSongs(uri, token) {
   while (uri != null) {
-    console.log(uri);
-    yield await fetch(uri, {method: "GET", headers: {Authorization: `Bearer ${token}`}}).then(resp => resp.json()).then(info => {
+    yield fetch(uri, {method: "GET", headers: {Authorization: `Bearer ${token}`}}).then(resp => resp.json()).then(info => {
       uri = info.next;
-      console.log(uri);
-      console.log(info);
       return info;
     });
   }
 };
 
-export function compileSongs(playlistIds, token, opts) {
-  for (let playlistId of playlistIds) {
-    let initialUri = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=total%2Cnext%2Citems%28track%28name%2Cid%2Cartists%28name%29%29%29`;
-    if (!opts.hasOwnProperty('info')) {
-      opts.info = [];
-    };
-
-    if (!opts.hasOwnProperty('logs')) {
-      opts.logs = getSongs(initialUri, token);
-    };
-
-    let { logs, info, onComplete } = opts;
-    let next = logs.next().value;
-    console.log(next);
-    console.log(logs);
-
-    if (next != undefined) { // issue now is that we aren't appending any info after the first call fucking bless
-      next.then(data => {
-        info = info.concat(data.items);
-        compileSongs(playlistId, token, {logs, info, onComplete});
-      });
-    } else {
-      onComplete(info);
-    }
+// this needs to be run as a forEach
+export function compileSongs(playlistId, token, opts) {
+  let initialUri = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=total%2Cnext%2Citems%28track%28name%2Cid%2Cartists%28name%29%29%29`;
+  
+  if (!opts.hasOwnProperty('info')) {
+    opts.info = [];
   };
+
+  if (!opts.hasOwnProperty('logs')) {
+    opts.logs = getSongs(initialUri, token);
+  };
+
+  let { logs, info, onComplete } = opts;
+  let next = logs.next().value;
+
+  if (next != undefined) {
+    next.then(data => {
+      info = info.concat(data);
+      compileSongs(playlistId, token, {logs, info, onComplete});
+    });
+  } else {
+    onComplete(info);
+  }
 }; 
   
 export async function getFeats(songs, token) {
